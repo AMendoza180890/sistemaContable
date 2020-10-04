4c8c2bf2eefc6acd4509616f76cd642542dcb7f4
AMendoza180890 <allanmaleman@gmail.com>
2020-10-04 12:12:01 -0600

    Se modificaron los botones de los edit para actualizar

--- resources/views/editimpresora.blade.php
+++ resources/views/editimpresora.blade.php
@@ -49,7 +49,7 @@
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-                            <button type="submit" class="btn btn-primary">Crear</button>
+                            <button type="submit" class="btn btn-primary">Actualizar</button>
                         </div>
                     </div>
                 </form>
