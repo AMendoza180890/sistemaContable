d0fac99482d6c57b28a6af456d201f3d9e5b8920
AMendoza180890 <allanmaleman@gmail.com>
2020-10-11 08:33:44 -0600

    Se puede exportar datos en diferentes formatos;

--- resources/js/ReporteActivo.js
+++ resources/js/ReporteActivo.js
@@ -10,31 +10,79 @@ $('#dateReporteActivo').on('change', function() {
     $('#tituloFechaSeleccionada').html("Reporte Realizado a la fecha " + fechaActual);
 
     $("td").each(function() {
-        // let costo = $('#costoActivo').attr('costo');
-        // let vidaUtil = $('#vidaUtilActivo').attr('vidaUtil');
-        // let fechaRecibido = $('#fechaRecibidoActivo').attr('fechaRecibido');
-        let val0 = $(this).attr('indice');
-        let val1 = $(this).attr("costo");
-        let val2 = $(this).attr("vidaUtil");
-        let val3 = $(this).attr("fechaRecibido");
+            // let costo = $('#costoActivo').attr('costo');
+            // let vidaUtil = $('#vidaUtilActivo').attr('vidaUtil');
+            // let fechaRecibido = $('#fechaRecibidoActivo').attr('fechaRecibido');
+            let val0 = $(this).attr('indice');
+            let val1 = $(this).attr("costo");
+            let val2 = $(this).attr("vidaUtil");
+            let val3 = $(this).attr("fechaRecibido");
 
-        console.log(val0);
-        console.log(val1);
-        console.log(val2);
-        console.log(val3);
+            console.log(val0);
+            console.log(val1);
+            console.log(val2);
+            console.log(val3);
 
-        //imprimir en documento
-        let indice = 'td#' + val0;
-        let indice2 = 'td#da' + val0;
-        let indice3 = 'td#sl' + val0;
-        // $("#MesDif").each(function() {
-        $(indice).text(mesDiferencia(val3));
-        $(indice2).text(formateo(depreciacionAcumuladaFunction(val3, val1, val2)));
-        $(indice3).text(formateo(saldoLibro(val3, val1, val2)));
-        // })
+            //imprimir en documento
+            let indice = 'td#' + val0;
+            let indice2 = 'td#da' + val0;
+            let indice3 = 'td#sl' + val0;
+            // $("#MesDif").each(function() {
+            $(indice).text(mesDiferencia(val3));
+            $(indice2).text(formateo(depreciacionAcumuladaFunction(val3, val1, val2)));
+            $(indice3).text(formateo(saldoLibro(val3, val1, val2)));
+            // })
+        }),
+        $('#Reporte').DataTable({
+            destroy: true,
+            dom: 'Bfrtip',
+            buttons: [{
+                    extend: 'copy',
+                    text: 'Copiar',
+                    exportOptions: {
+                        columns: ':visible',
+                        rows: ':visible'
+                    }
+                },
+                {
+                    extend: 'print',
+                    text: 'Imprimir',
+                    exportOptions: {
+                        columns: ':visible',
+                        rows: ':visible'
+                    }
+                },
+                {
+                    extend: 'csv',
+                    text: 'CSV',
+                    exportOptions: {
+                        columns: ':visible',
+                        rows: ':visible'
+                    }
+                },
+                {
+                    extend: 'excel',
+                    text: 'Excel',
+                    exportOptions: {
+                        columns: ':visible',
+                        rows: ':visible'
+                    }
+                },
+                {
+                    extend: 'pdf',
+                    text: 'PDF',
+                    exportOptions: {
+                        columns: ':visible',
+                        rows: ':visible'
+                    }
+                }
 
+            ],
+            responsive:  {                     breakpoints:  [{                             name:   'bigdesktop',                             width:  Infinity                         },                          {                             name:   'meddesktop',                             width:  1480                         },                          {                             name:   'smalldesktop',                             width:  1280                         },                          {                             name:   'medium',                             width:  1188                         },                          {                             name:   'tabletl',                             width:  1024                         },                          {                             name:   'btwtabllandp',                             width:  848                         },                          {                             name:   'tabletp',                             width:  768                         },                          {                             name:   'mobilel',                             width:  480                         },                          {                             name:   'mobilep',                             width:  320                         }                    ]                 },
+                            language:  {                     processing:   "Procesando",                     search:   "Buscar:",                     lengthMenu:   "Reporte de Activos",                     info:   "Elemento _START_ de _END_ en _TOTAL_ Total de elementos",                     infoEmpty:   "No se ha encontrado ningun elemento en lista",                     infoFiltered:   "Filtro de _MAX_ Cantidad total de elementos",                     infoPostFix:   "",                     loadingRecords:   "Espere un momento",                     zeroRecords:   "No se ha encontrado ningun elemento en lista",                     emptyTable:   "Aún no hay ningun elemento en lista",                     paginate:  {                         first:   "Primer",                         previous:   "Anterior",                         next:   "Siguiente",                         last:   "Ultimo"                     },                     buttons:  {                     copyTitle:   'Data copied',                     copyKeys:   'Use your keyboard or menu to select the copy command'                     }                 },
+             
 
-    });
+        });
 
     function formateo(resultado) {
         return 'C$ ' + resultado;
--- resources/views/RptActivoFijo.blade.php
+++ resources/views/RptActivoFijo.blade.php
@@ -1,12 +1,17 @@
 @extends('adminlte::page')
 
 @section('css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.bootstrap4.min.css"> --}}
+
+    {{--CSS BOTON EXPORTACION--}}
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
+    {{--FIN CSS EXPORTACION--}}
 @stop
 
-@section('title', 'TipoCuentas')
+@section('title', 'Reporte Activo Fijo')
 
 @section('content_header')
     <h1>Reporte de cuenta de Activo Fijo</h1>
@@ -143,72 +148,130 @@
 @stop
 
 @section('js')
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    {{--CND necesarios para exportar--}}
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.print.min.js"></script>
+    {{--Fin de CND de exportacion--}}
+
+    {{--CND necesarios para crear y dar formatos en DATATABLE--}}
+    {{-- <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
     <script src=" https://cdn.datatables.net/responsive/2.2.5/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.5/js/responsive.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.5/js/responsive.bootstrap4.min.js"></script> --}}
+    {{--fin de CND DATATABLE--}}
     <script>
-        $('#Reporte').DataTable({
-            responsive: {
-                breakpoints: [{
-                        name: 'bigdesktop',
-                        width: Infinity
-                    },
-                    {
-                        name: 'meddesktop',
-                        width: 1480
-                    },
-                    {
-                        name: 'smalldesktop',
-                        width: 1280
-                    },
-                    {
-                        name: 'medium',
-                        width: 1188
-                    },
-                    {
-                        name: 'tabletl',
-                        width: 1024
-                    },
-                    {
-                        name: 'btwtabllandp',
-                        width: 848
-                    },
-                    {
-                        name: 'tabletp',
-                        width: 768
-                    },
-                    {
-                        name: 'mobilel',
-                        width: 480
-                    },
-                    {
-                        name: 'mobilep',
-                        width: 320
-                    }
-                ]
-            },
-            language: {
-                processing: "Procesando",
-                search: "Buscar:",
-                lengthMenu: "Reporte de Activos",
-                info: "Elemento _START_ de _END_ en _TOTAL_ Total de elementos",
-                infoEmpty: "No se ha encontrado ningun elemento en lista",
-                infoFiltered: "Filtro de _MAX_ Cantidad total de elementos",
-                infoPostFix: "",
-                loadingRecords: "Espere un momento",
-                zeroRecords: "No se ha encontrado ningun elemento en lista",
-                emptyTable: "Aún no hay ningun elemento en lista",
-                paginate: {
-                    first: "Primer",
-                    previous: "Anterior",
-                    next: "Siguiente",
-                    last: "Ultimo"
+        $(document).ready(function(){
+            $('#Reporte').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                {extend: 'copy',
+                text: 'Copiar',
+                exportOptions: {
+                columns: ':visible',
+                    rows: ':visible'
                 }
-            }
-        });
+                },
+                {extend: 'print',
+                text:'Imprimir',
+                exportOptions: {
+                    columns: ':visible',
+                    rows: ':visible'
+                }
+                },
+                {extend: 'csv',
+                text:'CSV',
+                exportOptions: {
+                    columns: ':visible',
+                    rows: ':visible'
+                }
+                },
+                {extend: 'excel',
+                text: 'Excel',
+                exportOptions: {
+                    columns: ':visible',
+                    rows: ':visible'
+                }
+                },
+                {extend: 'pdf',
+                text: 'PDF',
+                exportOptions:{
+                    columns: ':visible',
+                    rows: ':visible'
+                }
+                }
+                ],
+                responsive: {
+                    breakpoints: [{
+                            name: 'bigdesktop',
+                            width: Infinity
+                        },
+                        {
+                            name: 'meddesktop',
+                            width: 1480
+                        },
+                        {
+                            name: 'smalldesktop',
+                            width: 1280
+                        },
+                        {
+                            name: 'medium',
+                            width: 1188
+                        },
+                        {
+                            name: 'tabletl',
+                            width: 1024
+                        },
+                        {
+                            name: 'btwtabllandp',
+                            width: 848
+                        },
+                        {
+                            name: 'tabletp',
+                            width: 768
+                        },
+                        {
+                            name: 'mobilel',
+                            width: 480
+                        },
+                        {
+                            name: 'mobilep',
+                            width: 320
+                        }
+                    ]
+                },
+                language: {
+                    processing: "Procesando",
+                    search: "Buscar:",
+                    lengthMenu: "Reporte de Activos",
+                    info: "Elemento _START_ de _END_ en _TOTAL_ Total de elementos",
+                    infoEmpty: "No se ha encontrado ningun elemento en lista",
+                    infoFiltered: "Filtro de _MAX_ Cantidad total de elementos",
+                    infoPostFix: "",
+                    loadingRecords: "Espere un momento",
+                    zeroRecords: "No se ha encontrado ningun elemento en lista",
+                    emptyTable: "Aún no hay ningun elemento en lista",
+                    paginate: {
+                        first: "Primer",
+                        previous: "Anterior",
+                        next: "Siguiente",
+                        last: "Ultimo"
+                    },
+                    buttons: {
+                    copyTitle: 'Data copied',
+                    copyKeys: 'Use your keyboard or menu to select the copy command'
+                    }
+                }, 
 
+        })
+    })
     </script>
     <script src="../../resources/js/ReporteActivo.js"></script>
 @stop
